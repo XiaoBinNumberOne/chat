3be8dc6cb97614f224c11e7ae7f0cacd2c447d1f
xiaobin <944484545@qq.com>
2019-11-28 22:53:44 +0800

    利用nacos作为注册中心

---
+++ im-framework/im-cloud-component/im-cloud-config/src/main/java/com/xbim/CloudConfigApp.java
@@ -0,0 +1,17 @@
+package com.xbim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 创建时间： 2019/11/28  22:39
+ * 备注：
+ **/
+@SpringBootApplication
+public class CloudConfigApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigApp.class);
+    }
+
+}
