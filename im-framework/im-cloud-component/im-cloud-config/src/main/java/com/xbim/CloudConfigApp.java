b8c8ab0e0626e4614b8fe6f5d57ac6fcb405c4d7
xiaobin <944484545@qq.com>
2019-11-30 16:27:49 +0800

    no message

--- im-framework/im-cloud-component/im-cloud-config/src/main/java/com/xbim/CloudConfigApp.java
+++ im-framework/im-cloud-component/im-cloud-config/src/main/java/com/xbim/CloudConfigApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 创建时间： 2019/11/28  22:39
- * 备注：连接nacos测试
+ * 备注：  连接 nacos配置中心 测试
  **/
 @SpringBootApplication
 public class CloudConfigApp {
