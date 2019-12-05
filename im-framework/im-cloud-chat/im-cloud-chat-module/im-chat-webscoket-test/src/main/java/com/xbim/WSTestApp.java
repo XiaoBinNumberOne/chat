1c4664a7c799e08d4c8e1b27e28bc3e57b02a11a
xiaobin <944484545@qq.com>
2019-12-05 22:49:27 +0800

    添加项目目录

---
+++ im-framework/im-cloud-chat/im-cloud-chat-module/im-chat-webscoket-test/src/main/java/com/xbim/WSTestApp.java
@@ -0,0 +1,16 @@
+package com.xbim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 创建时间： 2019/12/5  22:37
+ * 备注：
+ **/
+
+@SpringBootApplication
+public class WSTestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WSTestApp.class);
+    }
+}
