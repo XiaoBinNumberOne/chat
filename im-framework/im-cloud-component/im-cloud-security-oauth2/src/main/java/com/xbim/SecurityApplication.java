101406a797d205d26f2fcb1c35e7b0a908d95fea
宁斌 <ningbin@chaintor.com>
2020-01-16 11:39:06 +0800

    同时添加角色和权限

--- im-framework/im-cloud-component/im-cloud-security-oauth2/src/main/java/com/xbim/SecurityApplication.java
+++ im-framework/im-cloud-component/im-cloud-security-oauth2/src/main/java/com/xbim/SecurityApplication.java
@@ -4,8 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.Executors;
-
 @SpringBootApplication
 @MapperScan(basePackages = "com.xbim.mapper")
 public class SecurityApplication {
