2602ef7b7b1e52af30d7d69cc57187e252014348
XiaoBinNumberOne <944484545@qq.com>
2020-01-18 22:22:44 +0800

    前端添加protobuf

--- uniapp-chat/pages/tabbar/tabbar-1/tabbar-1.vue
+++ uniapp-chat/pages/tabbar/tabbar-1/tabbar-1.vue
@@ -1,10 +1,14 @@
 <template>
 	<view class="content">
-		页面 - 1
+		页面 - 12
+		<button @click="clickBtn()">点击</button>
 	</view>
 </template>
 
 <script>
+
+import ChatMessage from "../../../protobuf/ChatMessage_pb.js"
+	
 export default {
 	data() {
 		return {
@@ -12,7 +16,18 @@ export default {
 		};
 	},
 	onLoad() {},
-	methods: {}
+	methods: {
+		clickBtn(){
+			let chatMsg = new ChatMessage.chatMsg();
+			chatMsg.setMsgid("10001");
+			chatMsg.setFromid("001");
+			chatMsg.setToid("002");
+			chatMsg.setMsg("小斌你要加油，争取25岁之前有自己的事业");
+			console.log(chatMsg);
+			var bytes = chatMsg.serializeBinary();
+			console.log(ChatMessage.chatMsg.deserializeBinary(bytes));
+		}
+	}
 };
 </script>
 
