a69218cc1e68c7a81af28160a3a7ff544e618581
XiaoBinNumberOne <944484545@qq.com>
2020-02-15 19:38:52 +0800

    👾 uniapp添加登录页面

--- uniapp-chat/App.vue
+++ uniapp-chat/App.vue
@@ -1,17 +1,35 @@
 <script>
-export default {
-	onLaunch: function() {
-		console.log('App Launch');
-	},
-	onShow: function() {
-		console.log('App Show');
-	},
-	onHide: function() {
-		console.log('App Hide');
-	}
-};
+	export default {
+		onLaunch: function() {
+			console.log('App Launch');
+		},
+		onShow: function() {
+			console.log('App Show');
+		},
+		onHide: function() {
+			console.log('App Hide');
+		}
+	};
 </script>
 
 <style>
-/*每个页面公共css */ 
+	/*每个页面公共css */
+	* {
+		font-family: "Arial", "Microsoft YaHei", "黑体", "宋体", sans-serif;
+		padding: 0;
+		margin: 0;
+	}
+
+	page {
+		display: block;
+		width: 100%;
+		height: 100%;
+	}
+
+	.box-warpper {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 100%;
+	}
 </style>
